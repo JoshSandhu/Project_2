5dbf8f3c2cc094da02479a74b0b69380524ac534
JoshSandhu <joshsandhulindholm@gmail.com>
2022-04-06 14:37:33 +0000

    Added event listeners to start up after the DOM has loaded on JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Waiting for the DOM to finish loading
+// Get the button elements and add even listeners
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === 'restart') {
+                alert("You clicked Restart!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`)
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
@@ -11,5 +29,5 @@ function pickWinner() {
 }
 
 function incrementScore() {
-    
+
 }
