106bf777f7f76e27c5f6746c424bf9ad322dc283
JoshSandhu <joshsandhulindholm@gmail.com>
2022-04-06 11:30:00 +0000

    Added start of JS code with added variables for different options

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Logic of game inside
+let game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+}
+
+// Function for
+let playGame = () => {
+    let rockBtn = document.getElementsByClassName('rock');
+    let paperBtn = document.getElementsByClassName('paper');
+    let scissorBtn = document.getElementsByClassName('scissors');
+}
