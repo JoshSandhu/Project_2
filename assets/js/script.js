8198d7e9650c4d9e0ca80029d9e92eb2005af5eb
JoshSandhu <joshsandhulindholm@gmail.com>
2022-04-08 11:21:56 +0000

    Added alert for instructions on the instructions button

--- assets/js/script.js
+++ assets/js/script.js
@@ -161,5 +161,5 @@ function restartGame() {
 }
 
 function showInstructions() {
-
+    alert("Welcome to our Rock, Paper, Scissors, Lizard and Spock game. The rules are simple: Scissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors decapitates lizard, lizard eats paper, paper disproves Spock, Spock vaporizes rock, and as it always has, rock crushes scissors. Select your choice to start the game today.")
 }
